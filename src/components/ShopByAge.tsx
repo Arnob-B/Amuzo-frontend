b76bc2c852b0f01d04e3760165b0457c11aa1099
rahul-p19 <pandeyrahul.work@gmail.com>
2025-02-05 10:01:58 +0000

    shop by age and category

--- src/components/ShopByAge.tsx
+++ src/components/ShopByAge.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from "next/link";
+import Image from "next/image";
 
 function ShopByAge() {
   return (
@@ -7,22 +8,22 @@ function ShopByAge() {
       <h1 className='w-fit text-4xl sm:text-5xl font-bold'>Shop By Age</h1>
       <div className='grid grid-cols-2 sm:grid-cols-4 gap-y-4 gap-x-8 text-xl'>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=1" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/shopByAge1-3.webp")] bg-contain hover:scale-110'>
           </Link>
           <p>1-3 years</p>
         </div>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=3" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/shopByAge3.webp")] bg-contain hover:scale-110'>
           </Link>
           <p>3+ years</p>
         </div>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=4" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/shopByAge4.webp")] bg-contain hover:scale-110'>
           </Link>
           <p>4+ years</p>
         </div>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=8" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/shopByAge8.webp")] bg-contain hover:scale-110'>
           </Link>
           <p>8+ years</p>
         </div>
--- src/components/ShopByCategory.tsx
+++ src/components/ShopByCategory.tsx
@@ -7,12 +7,12 @@ function ShopByAge() {
       <h1 className='w-fit text-4xl sm:text-5xl font-bold text-center'>Shop By Category</h1>
       <div className='sm:w-1/4 grid grid-cols-2 gap-y-4 gap-x-8 text-center text-xl'>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=1" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/categoryEducation.jpg")] bg-contain hover:scale-110'>
           </Link>
           <p>Education and STEM</p>
         </div>
         <div className='flex flex-col items-center gap-y-3'>
-          <Link href="/product?age=3" className='flex flex-col items-center text-center bg-white rounded-full aspect-square border-gray-500 border border-dashed p-8 hover:bg-gray-200 transition-colors duration-150 h-full w-full'>
+          <Link href="/product?age=1" className='text-center rounded-full aspect-square border-gray-500 border border-dashed transition-all duration-300 h-full w-full bg-[url("/shopBy/categoryArts.svg")] bg-contain hover:scale-110'>
           </Link>
           <p>Arts and Crafts</p>
         </div>
